c5196be420d39a467d1cca082bdea1f073be16b2
Zeocdark <wisdom.wahyuaji@gmail.com>
2026-06-15 10:30:27 +0700

    perbaiki build css

--- check_db.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from './app/lib/prisma';
-async function main() {
-  console.log('Users:', await prisma.user.count());
-  console.log('Prodis:', await prisma.prodi.count());
-  console.log('Dosens:', await prisma.dosen.count());
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
