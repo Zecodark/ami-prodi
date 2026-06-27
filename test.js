b4728ba4f895e776868c08a7c9769ff48afda36c
Zeocdark <wisdom.wahyuaji@gmail.com>
2026-06-27 19:54:33 +0700

    clear debuging framework

--- test.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('./app/generated/prisma');
-const prisma = new PrismaClient();
-prisma.isianAmi.findMany().then(r => {
-  console.log(JSON.stringify(r.map(x => ({ id: x.id, status: x.status, dosen: x.dosen_id, unsur: x.pemeriksaan_unsur_id })), null, 2));
-}).catch(console.error).finally(() => prisma.$disconnect());
